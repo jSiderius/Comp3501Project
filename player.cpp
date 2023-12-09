a8c09a22be33f59898e3b65aa52759e135b9e5bb
Josh <jsiderius0@gmai.com>
2023-12-09 12:29:33 -0500

    turned player into heirarchal model

--- player.cpp
+++ player.cpp
@@ -2,7 +2,30 @@
 
 namespace game {
 
-Player::Player(const std::string name, const Resource *geometry, const Resource *material, const Resource *texture = NULL) : SceneNode(name, geometry, material, texture) {
+Player::Player(const std::string name, const Resource *geometry, const Resource *material, const Resource *texture, SceneNode **wheels, int num_wheels, SceneNode **antennas, int num_antennas) : SceneNode(name, geometry, material, texture) {
+    num_wheels_ = num_wheels;
+    num_antennas_ = num_antennas;
+
+    wheels_ = new SceneNode*[num_wheels_];
+    antennas_ = new SceneNode*[num_antennas_];
+    offsets_ = new glm::vec3[num_wheels];
+
+    
+    for (int i = 0; i < num_wheels_; i++){
+        wheels_[i] = wheels[i];
+    }
+    for (int i = 0; i < num_antennas_; i++){
+        antennas_[i] = antennas[i];
+    }
+
+    offsets_[0] = glm::vec3(-0.6, -0.2, -1.0);
+    offsets_[1] = glm::vec3(-0.6, -0.2, 0.0);
+    offsets_[2] = glm::vec3(-0.6, -0.2, 1.0);
+    offsets_[3] = glm::vec3(0.6, -0.2, -1.0);
+    offsets_[4] = glm::vec3(0.6, -0.2, 0.0);
+    offsets_[5] = glm::vec3(0.6, -0.2, 1.0);
+    offsets_[6] = glm::vec3(0.35, 0.6, 1.0);
+    offsets_[7] = glm::vec3(0.335, 1.2, 1.0);
 }
 
 
@@ -21,9 +44,37 @@ void Player::SetAngM(glm::quat angm){
     angm_ = angm;
 }
 
+void Player::Translate(glm::vec3 trans){
+    for (int i = 0; i < num_wheels_; i++){
+        wheels_[i]->Rotate(glm::angleAxis(0.05f, glm::vec3(GetOrientation() * glm::vec4(offsets_[i], 0.0f))));
+
+        // glm::vec3 offsetInWorldSpace = );
+        // wheels_[i]->SetPosition(GetPosition() + offsetInWorldSpace);
+        // wheels_[i]->Draw(camera);
+    }
+
+    SceneNode::Translate(trans);
+    Update();
+}
 
 void Player::Update(void){
     Rotate(angm_);
+
+    antennas_[1]->Rotate(glm::angleAxis(1.0f, glm::vec3(0,0.1,0))); 
+}
+
+void Player::Draw(Camera *camera){
+    SceneNode::Draw(camera);
+    for (int i = 0; i < num_wheels_; i++){
+        glm::vec3 offsetInWorldSpace = glm::vec3(GetOrientation() * glm::vec4(offsets_[i], 0.0f));
+        wheels_[i]->SetPosition(GetPosition() + offsetInWorldSpace);
+        wheels_[i]->Draw(camera);
+    }
+    for (int i = 0; i < num_antennas_; i++){
+        glm::vec3 offsetInWorldSpace = glm::vec3(GetOrientation() * glm::vec4(offsets_[num_wheels_ + i], 0.0f));
+        antennas_[i]->SetPosition(GetPosition() + offsetInWorldSpace);
+        antennas_[i]->Draw(camera);
+    }
 }
 
 void Player::Pitch(float angle){
@@ -33,7 +84,6 @@ void Player::Pitch(float angle){
 
 
 void Player::Yaw(float angle){
-
     glm::quat rotation = glm::angleAxis(angle, glm::vec3(0,1,0));
     SetOrientation(glm::normalize(GetOrientation() * rotation));
 }
